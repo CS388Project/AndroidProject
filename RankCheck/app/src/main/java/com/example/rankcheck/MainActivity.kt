840a3730dae1a43aaa742743ce32407cc6222d16
cristhian <cdml2001@hotmail.com>
2023-04-01 11:35:17 -0400

    set up DB connection

--- RankCheck/app/src/main/java/com/example/rankcheck/MainActivity.kt
+++ RankCheck/app/src/main/java/com/example/rankcheck/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.rankcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.ParseObject
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //DB Connection Test
+        /*
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","another one")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }*/
     }
+
 }
