a16c5cf6d33812ddede31fc3e385914ee858a28f
KasperKolasinski <kasper.kolasinski@gmail.com>
2023-04-03 16:06:10 -0400

    successful merge

---
+++ RankCheck/app/src/main/java/com/example/rankcheck/Registration.kt
@@ -0,0 +1,19 @@
+package com.example.rankcheck
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_page)
+
+        val retUser = findViewById<TextView>(R.id.txtRetUser)
+        retUser.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
